a8e1495cbbf94a85ff978df7967854ace53e2189
Rukhsar Ahmed <rukhsarnaich0@gmail.com>
2025-03-16 00:50:29 +0500

    Update MainBody.jsx

--- src/components/MainBody.jsx
+++ src/components/MainBody.jsx
@@ -4,7 +4,7 @@ import "./styles.css"
 const MainBody = () => {
   return (
     <div className="main-body">
-      <p className="joke">Why don't skeletons fight each other? Because they don't have the guts! 😆</p>
+      <p className="joke">Why don't skeletons fight each other? Because they don't have the guts! </p>
     </div>
   )
 }
